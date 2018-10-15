42dd458816b4d278d280ac20b0d411a5e918cffd
kk0walski <karol.kowalski95@gmail.com>
2018-10-15 11:42:15 +0200

    adding lack packages

---
+++ src/store/configureStore.jsx
@@ -0,0 +1,25 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import Board from "../reducers/Board";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+  const appReducer = combineReducers({
+    Board
+  });
+
+  const rootReducer = (state, action) => {
+    if (action.type === "LOG_OUT") {
+      state = undefined;
+    }
+
+    return appReducer(state, action);
+  };
+
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+  );
+  return store;
+};
