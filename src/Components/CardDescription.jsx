61744b70691166f651a5a99fecd2d1074a268717
kk0walski <karol.kowalski95@gmail.com>
2018-10-16 23:02:01 +0200

    Adding fields to card details

--- src/Components/CardDescription.jsx
+++ src/Components/CardDescription.jsx
@@ -1,7 +1,67 @@
 import React, { Component } from "react";
+import Markdown from "react-markdown";
+import Textarea from "react-textarea-autosize";
+import classnames from "classnames";
+import ClickOutside from "./ClickOutside";
+import { MdClose } from "react-icons/md";
 
 export default class CardDescription extends Component {
+  constructor() {
+    super();
+    this.openEditor.bind(this);
+    this.state = {
+      newText: "",
+      isOpen: false
+    };
+  }
+
+  openEditor = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   render() {
-    return <div />;
+    const { newText, isOpen } = this.state;
+    if (isOpen) {
+      return (
+        <ClickOutside handleClickOutside={this.openEditor}>
+          <Textarea
+            autoFocus
+            useCacheForDOMMeasurements
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+            value={newText}
+            className="card-adder-textarea"
+            placeholder="Dodaj bardziej szczegółowy opis..."
+            spellCheck={false}
+            style={{ minHeight: 108, marginTop: 5 }}
+            onBlur={this.openEditor}
+          />
+          <div className={classnames("edit-controls", "u-clearfix")}>
+            <input
+              className={classnames("primary", "mod-submit-edit")}
+              type="submit"
+              value="Zapisz"
+            />
+            <MdClose className="icon-x-lg" onClick={this.openEditor} />
+          </div>
+        </ClickOutside>
+      );
+    } else {
+      if (newText !== "") {
+        return (
+          <p className="u-bottom" onClick={this.openEditor}>
+            <Markdown escapeHtml={true} source={newText} />
+          </p>
+        );
+      } else {
+        return (
+          <p className="u-bottom" onClick={this.openEditor}>
+            <p className="card-detail-fake-text-area">
+              Dodaj bardziej szczegółowy opis...
+            </p>
+          </p>
+        );
+      }
+    }
   }
 }
--- src/Components/CardTitle.jsx
+++ src/Components/CardTitle.jsx
@@ -1,7 +1,71 @@
 import React, { Component } from "react";
+import ClickOutside from "./ClickOutside";
 
 export default class CardTitle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      newTitle: props.cardTitle
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ isOpen: true });
+  };
+
+  handleChange = event => {
+    this.setState({ newTitle: event.target.value });
+  };
+
+  submitTitle = () => {
+    const { cardTitle } = this.props;
+    const { newTitle } = this.state;
+    if (newTitle === "") return;
+    if (cardTitle !== newTitle) {
+      //this.props.changecardTitle(boardId, newTitle);
+    }
+    this.setState({ isOpen: false });
+  };
+
+  revertTitle = () => {
+    const { cardTitle } = this.props;
+    this.setState({ newTitle: cardTitle, isOpen: false });
+  };
+
+  handleKeyDown = event => {
+    if (event.keyCode === 13) {
+      this.submitTitle();
+    } else if (event.keyCode === 27) {
+      this.revertTitle();
+    }
+  };
+
+  handleFocus = event => {
+    event.target.select();
+  };
+
   render() {
-    return <div />;
+    const { isOpen, newTitle } = this.state;
+    const { cardTitle } = this.props;
+    return isOpen ? (
+      <ClickOutside handleClickOutside={this.revertTitle}>
+        <input
+          autoFocus
+          value={newTitle}
+          type="text"
+          onKeyDown={this.handleKeyDown}
+          onChange={this.handleChange}
+          onBlur={this.revertTitle}
+          onFocus={this.handleFocus}
+          className="mod-card-back-title"
+          spellCheck={false}
+        />
+      </ClickOutside>
+    ) : (
+      <button className="card-title-button" onClick={this.handleClick}>
+        <h2>{cardTitle}</h2>
+      </button>
+    );
   }
 }
